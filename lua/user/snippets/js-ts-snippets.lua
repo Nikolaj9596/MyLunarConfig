07feaa2a0a1fae004f0f9855b14d549bfd3e6e07
Nikolaj <nik.vagin1995@gmail.com>
2024-06-12 22:38:06 +0300

    feat: added new tsx react component snippet

--- lua/user/snippets/js-ts-snippets.lua
+++ lua/user/snippets/js-ts-snippets.lua
@@ -61,23 +61,27 @@ local function addSnippet(leng)
       { trig = "rc", snippetType = "snippet" },
       fmt(
         [[
-import React from "react";
+import React, {{ FC }} from "react";
+import {{ classNames }} from "shared";
 import cls from "./{}.module.scss";
 
-const {} = (props) => {{
+interface {}Props {{
+  className?: string;
+}}
+
+export const {}:FC<{}Props> = (props) => {{
   return (
-    <div className={{cls.{}}}>
+    <div className={{classNames(cls.{}, {{}}, [props.className ? props.className : ''])}}>
     </div>
   );
 }};
-
-export default {};
     ]],
         {
           i(1, "ComponentName"),
           rep(1),
-          i(2, "selectorName"),
           rep(1),
+          rep(1),
+          rep(1)
         }
       )
     ),
